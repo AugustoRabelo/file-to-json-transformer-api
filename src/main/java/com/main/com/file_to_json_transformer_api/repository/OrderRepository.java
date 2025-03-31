80ada99c7d04cd193135c1bd99602dde2ff4c697
Augusto Rabelo <augusto.cesar.furtado@gmail.com>
2025-03-31 16:59:58 -0300

    Adiciona novos metodos aos repositorios de order e product.

--- src/main/java/com/main/com/file_to_json_transformer_api/repository/OrderRepository.java
+++ src/main/java/com/main/com/file_to_json_transformer_api/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package com.main.com.file_to_json_transformer_api.repository;
 
 import com.main.com.file_to_json_transformer_api.entity.Order;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
     Optional<Order> findById(String id);
+
+    List<Order> findByDateBetween(LocalDate startDate, LocalDate endDate, Sort sort);
 }
--- src/main/java/com/main/com/file_to_json_transformer_api/repository/ProductRepository.java
+++ src/main/java/com/main/com/file_to_json_transformer_api/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package com.main.com.file_to_json_transformer_api.repository;
 
 import com.main.com.file_to_json_transformer_api.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(String id);
+
+    @Query("SELECT MAX(p.id) FROM Product p")
+    Integer findMaxId();
 }
